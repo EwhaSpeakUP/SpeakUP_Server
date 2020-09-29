16bc413df5d40d0672eae11ec05d63c50ca8cc51
Maeg <kimhj5854@naver.com>
2020-09-29 13:24:49 +0900

    revise /index/hwList code

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,4 +1,5 @@
 const { pool } = require('../../config/database');
+const { createBucket } = require('../../config/s3');
 //const { logger } = require('../../config/winston')
 
 /**---------- AssignList API ------------ */ 
@@ -6,38 +7,23 @@ const { pool } = require('../../config/database');
 //output: 해당 Class에 포함된 과제ID, 과제 이름
 
 exports.hwList = async function (req, res){
-    const classId = req.class_id;
+    
+    const classId = req.body.class_id;
+    const assignListParams = [classId];
 
-    try{ // try connection to DB
-        const connection = pool.getConnection(conn => conn);
-        try { // try query execution
-            const assignListQuery = " SELECT HW_ID, HW_NAME FROM HOMEWORK AS H WHERE H.CLASS_ID = ?";
-            const assignListParams = [classId];
-            const [assignListRows] = await conn.query(assignListQuery, assignListParams);
-            
+    const assignListQuery = " SELECT HW_ID, HW_NAME FROM HOMEWORK AS H WHERE H.CLASS_ID = ?";
+    pool.query(assignListQuery, assignListParams, function(error, results, fields){
+        if (error) throw error;
+        else{
             return res.json({
                 isSuccess : true,
                 code: 100,
                 message: "과제목록",
                 result: {
-                    hw_ids : assignListRows
+                    hw_ids : results
                 }
-            });    
+            });
         }
-        catch(err){
-            console.log(err);
-            return res.json({
-                isSuccess : false,
-                code: 201,
-                message: "DB 쿼리 수행에 문제가 있습니다.",
-            });    
-        }
-    }
-    catch (err) { // connection error
-        return res.json({
-            isSuccess : false,
-            code: 200,
-            message: "DB 연결에 실패했습니다."
-        });
-    }
+    });
 };
+

5c97ee6465abfdcc4fd77194941a218e96db5605
Maeg <kimhj5854@naver.com>
2020-10-04 00:01:59 +0900

    transmitFile API method modified : POST > GET

--- src/routes/assignRoute.js
+++ src/routes/assignRoute.js
@@ -2,6 +2,6 @@ module.exports = function(app) {
     const assign = require("../controllers/assignController");
     
     //app.use('/assign/uploadAssign',assign.);
-    app.post("/assign/:hwID/:stID", assign.uploadS3.single('file'),assign.uploadAssign); // 과제 음성 업로드 (학생)
-    app.post("/assign/transmitFile",assign.transmitFile);
+    app.post("/assign/:hwID/:stID", assign.uploadS3.single('file'),assign.uploadAssign); // 통역 음성 업로드 (학생)
+    app.get("/assign/:hwID",assign.transmitFile); // 연사 음성 다운로드 (학생)
 };

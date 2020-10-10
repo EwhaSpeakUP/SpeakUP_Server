8dfb4ae60b4af86ed2fa4290443c4eaea2821e7e
RLADUDWN7237 <72kyj37@naver.com>
2020-10-10 23:05:11 +0900

    transmitFile 수정

--- src/controllers/assignController.js
+++ src/controllers/assignController.js
@@ -51,17 +51,18 @@ exports.uploadAssign = async function (req, res){
 
 exports.transmitFile = function(req,res){
     
-    var hw_id = req.params.hwID;
-    var sql = "SELECT ORIGIN_VOICE FROM HOMEWORK WHERE HW_ID=?";
-       pool.query(sql, [hw_id], function(err, result){
+    var assign_id = req.params.assignID;
+    var sql = "SELECT ORIGIN_VOICE FROM ASSIGNMENT WHERE ASSIGNMENT_ID=?";
+       pool.query(sql, [assign_id], function(err, result){
                   
           if(err) throw err;
           else{
+                var file_link=result[0]["ORIGIN_VOICE"].split('|');;
                 var result={
                     isSuccess : true,
                     code : 100,
                     message : "파일 수신에 성공했습니다.",
-                    result : {filepath : result[0]['ORIGIN_VOICE']}
+                    result : {filepath : file_link}
                 };
                 res.writeHead(200, {'Content-Type':'application/json/json'});
                 res.end(JSON.stringify(result));

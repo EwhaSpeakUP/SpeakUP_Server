f9ef36c07dab8341dbfc11dcde2bc6a1f54d8d09
RLADUDWN7237 <72kyj37@naver.com>
2020-10-10 23:09:49 +0900

    route 변경

--- src/routes/assignRoute.js
+++ src/routes/assignRoute.js
@@ -3,5 +3,5 @@ module.exports = function(app) {
     
     //app.use('/assign/uploadAssign',assign.);
     app.post("/assign/:hwID/:stID", assign.uploadS3.single('file'),assign.uploadAssign); // 통역 음성 업로드 (학생)
-    app.get("/assign/:hwID",assign.transmitFile); // 연사 음성 다운로드 (학생)
+    app.get("/assign/:assignID",assign.transmitFile); // 연사 음성 다운로드 (학생)
 };

274a9c7c73b82a71c13a6ffb4c12f935949df0a0
EC2 Default User <ec2-user@ip-172-31-45-229.ap-northeast-2.compute.internal>
2020-11-08 13:31:40 +0000

    revise assign Route

--- src/routes/assignRoute.js
+++ src/routes/assignRoute.js
@@ -1,10 +1,10 @@
 module.exports = function(app) {
-    //const assign = require("../controllers/assignController");
-    const assign = require("../../temp1");
+    const assign = require("../controllers/assignController");
+    //const assign = require("../../temp1");
     const auth = require("../../auth");
 
     app.post("/assign/:assignID", auth, assign.uploadAssign); // 통역 음성 업로드 (학생)
-    //app.get("/assign/:assignID", assign.transmitFile); // 연사 음성 학생에게 전송
+    app.get("/assign/:assignID", assign.transmitFile); // 연사 음성 학생에게 전송
 
 
 };
